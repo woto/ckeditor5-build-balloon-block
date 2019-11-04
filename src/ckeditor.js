6cebb34e9c87b633a31788aa4333cbe000cb109a
Ruslan Kornev <oganer@gmail.com>
2019-11-04 23:05:14 +0300

    Adds autosave and upload

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -30,6 +30,9 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+
 import '../theme/theme.css';
 
 export default class BalloonEditor extends BalloonEditorBase {}
@@ -58,7 +61,9 @@ BalloonEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	Autosave,
+	SimpleUploadAdapter
 ];
 
 // Editor configuration.
